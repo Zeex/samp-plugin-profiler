5a3fa6ecb76d6f436c1d7c0d2bc0e03a4cdaba6b
Zeex <zeex@rocketmail.com>
2011-11-11 04:40:56 +0600

    Switch from STLsoft to Boost.Chrono

--- src/perfcounter.h
+++ src/perfcounter.h
@@ -1,19 +1,36 @@
+// Copyright (c) 2011 Zeex
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef PERFCOUNTER_H
 #define PERFCOUNTER_H
 
-#include <platformstl/platformstl.hpp>
-#include <platformstl/performance/performance_counter.hpp>
+#include <cstdint>
+
+#include <boost/chrono.hpp>
 
 class PerformanceCounter {
 public:
+	typedef boost::chrono::system_clock Clock;
+
 	PerformanceCounter();
 	~PerformanceCounter();
 
 	void Start(PerformanceCounter *parent = 0);
 	void Stop();
 
-	platformstl::int64_t GetCalls() const;
-	platformstl::int64_t GetTime() const;
+	std::int64_t GetNumberOfCalls() const;
+	std::int64_t GetTotalTime() const;
 
 private:
 	void Pause();
@@ -25,9 +42,10 @@ private:
 	PerformanceCounter *child_;
 	PerformanceCounter *parent_;
 
-	platformstl::int64_t calls_;
-	platformstl::performance_counter counter_;
-	platformstl::performance_counter::interval_type time_;
+	std::int64_t num_calls_;
+
+	Clock::time_point start_;
+	Clock::duration   total_time_;
 };
 
 #endif
