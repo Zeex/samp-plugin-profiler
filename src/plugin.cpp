70d29d74460ab3e4ce8ed6be02924f6abcaafcb3
Sergey Zolotarev <zeex@rocketmail.com>
2011-12-11 08:46:58 +0600

    Add ability to enabled/disable script profiling via the  'profiler_enabled' public variable

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -40,26 +40,22 @@
 
 #include "amx/amx.h"
 
-using namespace samp_profiler;
-
-extern void *pAMXFunctions; 
-
-namespace {
-
 typedef void (*logprintf_t)(const char *format, ...);
 
-logprintf_t logprintf;
+// AMX API functons array
+extern void *pAMXFunctions; 
+
+// For logging
+static logprintf_t logprintf;
 
 // Symbolic info, used for getting function names
-std::map<AMX*, DebugInfo> debugInfos;
+static std::map<AMX*, samp_profiler::DebugInfo> debugInfos;
 
-// x86 is Little Endian...
-void *AMXAPI DummyAmxAlign(void *v) { return v; }
+// Hooks
+static samp_profiler::JumpX86 ExecHook;
+static samp_profiler::JumpX86 CallbackHook;
 
-JumpX86 ExecHook;
-JumpX86 CallbackHook;
-
-int AMXAPI Exec(AMX *amx, cell *retval, int index) {
+static int AMXAPI Exec(AMX *amx, cell *retval, int index) {
 	ExecHook.Remove();		
 	CallbackHook.Install(); // P-code may call natives 
 
@@ -67,7 +63,7 @@ int AMXAPI Exec(AMX *amx, cell *retval, int index) {
 	int error = AMX_ERR_NONE;
 
 	// Check if this script has a profiler attached to it
-	Profiler *prof = Profiler::Get(amx);
+	samp_profiler::Profiler *prof = samp_profiler::Profiler::Get(amx);
 	if (prof != 0) {
 		error =  prof->Exec(retval, index);
 	} else {
@@ -80,7 +76,7 @@ int AMXAPI Exec(AMX *amx, cell *retval, int index) {
 	return error;
 }
 
-int AMXAPI Callback(AMX *amx, cell index, cell *result, cell *params) {
+static int AMXAPI Callback(AMX *amx, cell index, cell *result, cell *params) {
 	CallbackHook.Remove();		
 	ExecHook.Install(); // Natives may call amx_Exec() 
 
@@ -92,7 +88,7 @@ int AMXAPI Callback(AMX *amx, cell index, cell *result, cell *params) {
 	int error = AMX_ERR_NONE;
 
 	// Check if this script has a profiler attached to it
-	Profiler *prof = Profiler::Get(amx);
+	samp_profiler::Profiler *prof = samp_profiler::Profiler::Get(amx);
 	if (prof != 0) {
 		error =  prof->Callback(index, result, params);
 	} else {
@@ -106,27 +102,36 @@ int AMXAPI Callback(AMX *amx, cell index, cell *result, cell *params) {
 }
 
 // Replaces back slashes with forward slashes
-std::string ToPortablePath(const std::string &path) {
+static std::string ToNormalPath(const std::string &path) {
 	std::string fsPath = path;
 	std::replace(fsPath.begin(), fsPath.end(), '\\', '/');   
 	return fsPath;
 }
 
-bool IsGameMode(const std::string &amxName) {
-	return ToPortablePath(amxName).find("gamemodes/") != std::string::npos;
+static bool IsGameMode(const std::string &amxName) {
+	return ToNormalPath(amxName).find("gamemodes/") != std::string::npos;
 }
 
-bool IsFilterScript(const std::string &amxName) {
-	return ToPortablePath(amxName).find("filterscripts/") != std::string::npos;
+static bool IsFilterScript(const std::string &amxName) {
+	return ToNormalPath(amxName).find("filterscripts/") != std::string::npos;
+}
+
+static bool GetPublicVariable(AMX *amx, const char *name, cell &value) {
+	cell  amx_addr;
+	if (amx_FindPubVar(amx, name, &amx_addr) == AMX_ERR_NONE) {
+		cell *phys_addr;
+		amx_GetAddr(amx, amx_addr, &phys_addr);
+		value = *phys_addr;
+		return true; 
+	}
+	return false;
 }
 
 // Returns true if the .amx should be profiled
-bool WantsProfiler(const std::string &amxName) {
-	std::string goodAmxName = ToPortablePath(amxName);
+static bool WantsProfiler(const std::string &amxName) {
+	std::string goodAmxName = ToNormalPath(amxName);
 
-	// This only works if they place their gamemodes and filterscripts in default directories.
-	// Someting like ../my_scripts/awesome_script.amx obviously won't work.
-	ConfigReader server_cfg("server.cfg");
+	samp_profiler::ConfigReader server_cfg("server.cfg");
 	if (IsGameMode(amxName)) {
 		// This is a gamemode
 		if (server_cfg.GetOption("profile_gamemode", false)) {
@@ -148,11 +153,12 @@ bool WantsProfiler(const std::string &amxName) {
 	return false;
 }
 
-} // namespace
-
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
 	return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
 }
+
+// x86 is Little Endian...
+static void *AMXAPI my_amx_Align(void *v) { return v; }
 
 PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
 	pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
@@ -160,9 +166,9 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
 
 	// The server does not export amx_Align* for some reason.
 	// They are used in amxdbg.c and amxaux.c, so they must be callable.
-	((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Align16] = (void*)DummyAmxAlign; // amx_Align16
-	((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Align32] = (void*)DummyAmxAlign; // amx_Align32
-	((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Align64] = (void*)DummyAmxAlign; // amx_Align64
+	((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Align16] = (void*)my_amx_Align; // amx_Align16
+	((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Align32] = (void*)my_amx_Align; // amx_Align32
+	((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Align64] = (void*)my_amx_Align; // amx_Align64
 
 	ExecHook.Install(
 		((void**)pAMXFunctions)[PLUGIN_AMX_EXPORT_Exec], 
@@ -181,32 +187,38 @@ PLUGIN_EXPORT void PLUGIN_CALL Unload() {
 }
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx) {
-	std::string filename = GetAmxName(amx);
+	std::string filename = samp_profiler::GetAmxName(amx);
 	if (filename.empty()) {
 		logprintf("Profiler: Failed to detect .amx name, prifiling will not be done");
 		return AMX_ERR_NONE;
 	}
 
-	if (!Profiler::IsScriptProfilable(amx)) {
+	if (!samp_profiler::Profiler::IsScriptProfilable(amx)) {
 		logprintf("Profiler: Can't profile script %s (are you using -d0?)", filename.c_str());
 		return AMX_ERR_NONE;
 	}
 	 
-	if (WantsProfiler(filename)) {
-		if (DebugInfo::HasDebugInfo(amx)) {
-			DebugInfo debugInfo;
+	cell profiler_enabled = false; 
+	if (GetPublicVariable(amx, "profiler_enabled", profiler_enabled) 
+			&& !profiler_enabled) {
+		return AMX_ERR_NONE;
+	}
+
+	if (profiler_enabled || WantsProfiler(filename)) {
+		if (samp_profiler::DebugInfo::HasDebugInfo(amx)) {
+			samp_profiler::DebugInfo debugInfo;
 			debugInfo.Load(filename);
 			if (debugInfo.IsLoaded()) {
 				logprintf("Profiler: Loaded debug info from %s", filename.c_str());
 				::debugInfos[amx] = debugInfo;				
-				Profiler::Attach(amx, debugInfo); 
+				samp_profiler::Profiler::Attach(amx, debugInfo); 
 				logprintf("Profiler: Attached profiler instance to %s", filename.c_str());
 				return AMX_ERR_NONE;
 			} else {
 				logprintf("Profiler: Error loading debug info from %s", filename.c_str());
 			}
 		}		
-		Profiler::Attach(amx);
+		samp_profiler::Profiler::Attach(amx);
 		logprintf("Profiler: Attached profiler instance to %s (no debug symbols)", filename.c_str());
 	} 	
 
@@ -215,31 +227,31 @@ PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx) {
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx) {
 	// Get an instance of Profiler attached to the unloading AMX
-	Profiler *prof = Profiler::Get(amx);
+	samp_profiler::Profiler *prof = samp_profiler::Profiler::Get(amx);
 
 	// Detach profiler
 	if (prof != 0) {
-		std::string amx_path = GetAmxName(amx);
+		std::string amx_path = samp_profiler::GetAmxName(amx); // must be in cache
 		std::string amx_name = std::string(amx_path, 0, amx_path.find_last_of("."));
 
 		// Output stats depending on currently set output_format
-		ConfigReader server_cfg("server.cfg");
+		samp_profiler::ConfigReader server_cfg("server.cfg");
 
 		std::string format = 
 			server_cfg.GetOption("profile_format", std::string("html"));
 
 		std::string filename;
-		AbstractPrinter *printer = 0;
+		samp_profiler::AbstractPrinter *printer = 0;
 
 		if (format == "html") {			
 			filename = amx_name + "-profile.html";
-			printer = new HtmlPrinter;			
+			printer = new samp_profiler::HtmlPrinter;			
 		} else if (format == "text") {
 			filename = amx_name + "-profile.txt";
-			printer = new TextPrinter;
+			printer = new samp_profiler::TextPrinter;
 		} else if (format == "xml") {
 			filename = amx_name + "-profile.xml";
-			printer = new XmlPrinter;
+			printer = new samp_profiler::XmlPrinter;
 		} else {
 			logprintf("Profiler: Unknown output format '%s'", format.c_str());
 		}
@@ -248,11 +260,11 @@ PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx) {
 		prof->PrintStats(ostream, printer);
 		delete printer;
 
-		Profiler::Detach(amx);
+		samp_profiler::Profiler::Detach(amx);
 	}
 
 	// Free debug info
-	std::map<AMX*, DebugInfo>::iterator it = ::debugInfos.find(amx);
+	std::map<AMX*, samp_profiler::DebugInfo>::iterator it = ::debugInfos.find(amx);
 	if (it != ::debugInfos.end()) {
 		it->second.Free();
 		::debugInfos.erase(it);
