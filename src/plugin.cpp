Zeex/samp-plugin-profiler

commit bcd13beffbe8bef1db5be90e96038230eb3e2ab6

Refactor plugin code

src/plugin.cpp
@@ -22,173 +22,37 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <exception>
-#include <fstream>
-#include <list>
-#include <map>
-#include <sstream>
-#include <string>
 #include <subhook.h>
-#include <amx/amx.h>
-#include <amxprof/call_graph_writer_dot.h>
-#include <amxprof/debug_info.h>
-#include <amxprof/statistics_writer_html.h>
-#include <amxprof/statistics_writer_text.h>
-#include <amxprof/statistics_writer_json.h>
-#include <amxprof/profiler.h>
-#include "amxpath.h"
-#include "configreader.h"
+#include "logprintf.h"
 #include "plugin.h"
 #include "pluginversion.h"
+#include "profiler.h"
 
 typedef void (*logprintf_t)(const char *format, ...);
 
 extern void *pAMXFunctions;
 
-static logprintf_t logprintf;
-
-typedef std::map<AMX*, AMX_DEBUG> AmxToAmxDebugMap;
-static AmxToAmxDebugMap old_debug_hooks;
-
-typedef std::map<AMX*, amxprof::Profiler*> AmxToProfilerMap;
-static AmxToProfilerMap profilers;
-
-typedef std::map<AMX*, amxprof::DebugInfo*> AmxToDebugInfoMap; 
-static AmxToDebugInfoMap debug_infos;
-
-// Plugin settings and their defauls.
-namespace cfg {
-  bool          profile_gamemode      = false;
-  std::string   profile_filterscripts = "";
-  std::string   profile_format        = "html";
-  bool          call_graph            = false;
-  std::string   call_graph_format     = "dot";
-}
-
-static void PrintException(const std::exception &e) {
-  logprintf("[profiler] Error: %s", e.what());
-}
-
 namespace hooks {
 
-SubHook amx_Exec_hook;
-SubHook amx_Callback_hook;
+SubHook exec_hook;
 
 int AMXAPI amx_Debug(AMX *amx) {
-  amxprof::Profiler *profiler = ::profilers[amx];
-  if (profiler) {
-    try {
-      profiler->DebugHook();
-    } catch (const std::exception &e) {
-      PrintException(e);
-    }
-  }
-
-  AmxToAmxDebugMap::const_iterator iterator = old_debug_hooks.find(amx);
-  if (iterator != old_debug_hooks.end()) {
-    if (iterator->second != 0) {
-      return (iterator->second)(amx);
-    }
-  }
-
-  return AMX_ERR_NONE;
+  return Profiler::GetInstance(amx)->Debug();
 }
 
 int AMXAPI amx_Callback(AMX *amx, cell index, cell *result, cell *params) {
-  SubHook::ScopedRemove r(&amx_Callback_hook);
-  SubHook::ScopedInstall i(&amx_Exec_hook);
-
-  amxprof::Profiler *profiler = ::profilers[amx];
-  if (profiler != 0) {
-    try {
-      return profiler->CallbackHook(index, result, params);
-    } catch (const std::exception &e) {
-      PrintException(e);
-    }
-  }
-
-  return ::amx_Callback(amx, index, result, params);
+  return Profiler::GetInstance(amx)->Callback(index, result, params);
 }
 
 int AMXAPI amx_Exec(AMX *amx, cell *retval, int index) {
-  SubHook::ScopedRemove r(&amx_Exec_hook);
-  SubHook::ScopedInstall i(&amx_Callback_hook);
-
-  amxprof::Profiler *profiler = ::profilers[amx];
-  if (profiler != 0) {
-    try {
-      return profiler->ExecHook(retval, index);
-    } catch (const std::exception &e) {
-      PrintException(e);
-    }
+  SubHook::ScopedRemove _(&exec_hook);
+  if (amx->flags & AMX_FLAG_BROWSE) {
+    return ::amx_Exec(amx, retval, index);
   }
-
-  return ::amx_Exec(amx, retval, index);
+  return Profiler::GetInstance(amx)->Exec(retval, index);
 }
 
 } // namespace hooks
-
-static void ToLower(std::string &s) {
-  std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-}
-
-static std::string GetAmxPath(AMX *amx) {
-  // Has to be static to make caching work in AmxUnload().
-  static AmxPathFinder finder;
-  finder.AddSearchDirectory("gamemodes");
-  finder.AddSearchDirectory("filterscripts");
-  return finder.FindAmxPath(amx);
-}
-
-static std::string ToUnixPath(const std::string &path) {
-  std::string fsPath = path;
-  std::replace(fsPath.begin(), fsPath.end(), '\\', '/');
-  return fsPath;
-}
-
-static bool IsGameMode(const std::string &amx_name) {
-  return ToUnixPath(amx_name).find("gamemodes/") != std::string::npos;
-}
-
-static bool IsFilterScript(const std::string &amx_name) {
-  return ToUnixPath(amx_name).find("filterscripts/") != std::string::npos;
-}
-
-static bool WantsProfiler(const std::string &amx_name) {
-  std::string good_amx_name = ToUnixPath(amx_name);
-
-  if (IsGameMode(good_amx_name)) {
-    if (cfg::profile_gamemode) {
-      return true;
-    }
-  } else if (IsFilterScript(good_amx_name)) {
-    std::string fs_list = cfg::profile_filterscripts;
-    std::stringstream fs_stream(fs_list);
-    do {
-      std::string fs_name;
-      fs_stream >> fs_name;
-      if (good_amx_name == "filterscripts/" + fs_name + ".amx"
-          || good_amx_name == "filterscripts/" + fs_name) {
-        return true;
-      }
-    } while (!fs_stream.eof());
-  }
-
-  return false;
-}
-
-template<typename Map, typename Key>
-static void DeleteMapEntry(Map &map, const Key &key) {
-  typename Map::iterator iterator = map.find(key);
-  if (iterator != map.end()) {
-    delete iterator->second;
-    map.erase(iterator);
-  }
-}
 
 template<typename Func>
 static void *FunctionToVoidPtr(Func func) {
@@ -204,175 +68,31 @@ PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
 }
 
 PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
-  try {
-    pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
-    logprintf = (logprintf_t) ppData[PLUGIN_DATA_LOGPRINTF];
+  pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
+  logprintf = (logprintf_t)ppData[PLUGIN_DATA_LOGPRINTF];
 
-    void **exports = reinterpret_cast<void**>(pAMXFunctions);
+  void **exports = reinterpret_cast<void**>(pAMXFunctions);
 
-    exports[PLUGIN_AMX_EXPORT_Align16] = FunctionToVoidPtr(amx_Align_stub);
-    exports[PLUGIN_AMX_EXPORT_Align32] = FunctionToVoidPtr(amx_Align_stub);
-    exports[PLUGIN_AMX_EXPORT_Align64] = FunctionToVoidPtr(amx_Align_stub);
+  exports[PLUGIN_AMX_EXPORT_Align16] = FunctionToVoidPtr(amx_Align_stub);
+  exports[PLUGIN_AMX_EXPORT_Align32] = FunctionToVoidPtr(amx_Align_stub);
+  exports[PLUGIN_AMX_EXPORT_Align64] = FunctionToVoidPtr(amx_Align_stub);
 
-    hooks::amx_Exec_hook.SetSrc(exports[PLUGIN_AMX_EXPORT_Exec]);
-    hooks::amx_Exec_hook.SetDst(FunctionToVoidPtr(hooks::amx_Exec));
-    hooks::amx_Exec_hook.Install();
+  hooks::exec_hook.SetSrc(exports[PLUGIN_AMX_EXPORT_Exec]);
+  hooks::exec_hook.SetDst(FunctionToVoidPtr(hooks::amx_Exec));
+  hooks::exec_hook.Install();
 
-    hooks::amx_Callback_hook.SetSrc(exports[PLUGIN_AMX_EXPORT_Callback]);
-    hooks::amx_Callback_hook.SetDst(FunctionToVoidPtr(hooks::amx_Callback));
-
-    ConfigReader server_cfg("server.cfg");
-    server_cfg.GetOption("profile_gamemode", cfg::profile_gamemode);
-    server_cfg.GetOption("profile_filterscripts", cfg::profile_filterscripts);
-    server_cfg.GetOption("profile_format", cfg::profile_format);
-    server_cfg.GetOption("call_graph", cfg::call_graph);
-    server_cfg.GetOption("call_graph_format", cfg::call_graph_format);
-
-    logprintf("  Profiler v" PROJECT_VERSION_STRING " is OK.");
-  }
-  catch (std::exception &e) {
-    PrintException(e);
-  }
-
+  logprintf("  Profiler v" PROJECT_VERSION_STRING " is OK.");
   return true;
 }
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx) {
-  try {
-    std::string filename = GetAmxPath(amx);
-
-    if (filename.empty()) {
-      logprintf("[profiler] Failed to find corresponding .amx file");
-      return AMX_ERR_NONE;
-    }
-
-    if (!WantsProfiler(filename)) {
-      return AMX_ERR_NONE;
-    }
-
-    amxprof::DebugInfo *debug_info = 0;
-
-    if (amxprof::HasDebugInfo(amx)) {
-      debug_info = new amxprof::DebugInfo(filename);
-      if (debug_info->is_loaded()) {
-        ::debug_infos[amx] = debug_info;
-      } else {
-        logprintf("[profiler] Error loading debug info: %s",
-                  aux_StrError(debug_info->last_error()));
-        delete debug_info;
-        debug_info = 0;
-      }
-    }
-
-    amxprof::Profiler *profiler = new amxprof::Profiler(amx,
-                                                                  debug_info);
-    profiler->set_call_graph_enabled(cfg::call_graph);
-
-    if (debug_info != 0) {
-      logprintf("[profiler] Attached profiler to '%s'", filename.c_str());
-    } else {
-      logprintf("[profiler] Attached profiler to '%s' (no debug info)",
-                filename.c_str());
-    }
-
-    ::old_debug_hooks[amx] = amx->debug;
-    amx_SetDebugHook(amx, hooks::amx_Debug);
-
-    ::profilers[amx] = profiler;
-  }
-  catch (const std::exception &e) {
-    PrintException(e);
-  }
-
+  Profiler::CreateInstance(amx);
+  amx_SetDebugHook(amx, hooks::amx_Debug);
+  amx_SetCallback(amx, hooks::amx_Callback);
   return AMX_ERR_NONE;
 }
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx) {
-  try {
-    amxprof::Profiler *profiler = ::profilers[amx];
-
-    if (profiler != 0) {
-      std::string amx_path = GetAmxPath(amx);
-      std::string amx_name = std::string(amx_path, 0,
-                                         amx_path.find_last_of("."));
-
-      ToLower(cfg::profile_format);
-      std::string profile_filename = amx_name + "-profile." +
-                                     cfg::profile_format;
-      std::ofstream profile_stream(profile_filename.c_str());
-
-      if (profile_stream.is_open()) {
-        amxprof::StatisticsWriter *writer = 0;
-
-        if (cfg::profile_format == "html") {
-          writer = new amxprof::StatisticsWriterHtml;
-        } else if (cfg::profile_format == "txt" ||
-                   cfg::profile_format == "text") {
-          writer = new amxprof::StatisticsWriterText;
-        } else if (cfg::profile_format == "json") {
-          writer = new amxprof::StatisticsWriterJson;
-        } else {
-          logprintf("[profiler] Unrecognized profile format '%s'",
-                    cfg::profile_format.c_str());
-        }
-
-        if (writer != 0) {
-          logprintf("[profiler] Writing profile to '%s'",
-                    profile_filename.c_str());
-          writer->set_stream(&profile_stream);
-          writer->set_script_name(amx_path);
-          writer->set_print_date(true);
-          writer->set_print_run_time(true);
-          writer->Write(profiler->stats());
-          delete writer;
-        }
-
-        profile_stream.close();
-      } else {
-        logprintf("[profiler]: Error opening file '%s'",
-                  profile_filename.c_str());
-      }
-
-      if (cfg::call_graph) {
-        ToLower(cfg::call_graph_format);
-        std::string call_graph_filename = amx_name + "-calls." +
-                                          cfg::call_graph_format;
-        std::ofstream call_graph_stream(call_graph_filename.c_str());
-
-        if (call_graph_stream.is_open()) {
-          amxprof::CallGraphWriterDot *writer = 0;
-
-          if (cfg::call_graph_format == "dot") {
-            writer = new amxprof::CallGraphWriterDot;
-          } else {
-            logprintf("[profiler] Unrecognized call graph format '%s'",
-                      cfg::call_graph_format.c_str());
-          }
-
-          if (writer != 0) {
-            logprintf("[profiler] Writing call graph to '%s'",
-                      call_graph_filename.c_str());
-            writer->set_stream(&call_graph_stream);
-            writer->set_script_name(amx_path);
-            writer->set_root_node_name("SA-MP Server");
-            writer->Write(profiler->call_graph());
-            delete writer;
-          }
-
-          call_graph_stream.close();
-        } else {
-          logprintf("[profiler]: Error opening file '%s'",
-                    call_graph_filename.c_str());
-        }
-      }
-    }
-
-    DeleteMapEntry(::profilers, amx);
-    DeleteMapEntry(::debug_infos, amx);
-  }
-  catch (const std::exception &e) {
-    PrintException(e);
-  }
-
+  Profiler::DestroyInstance(amx);
   return AMX_ERR_NONE;
 }

src/profiler.cpp
@@ -1,0 +1,276 @@
+// Copyright (c) 2011-2014 Zeex
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <algorithm>
+#include <cstdarg>
+#include <exception>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <amxprof/call_graph_writer_dot.h>
+#include <amxprof/debug_info.h>
+#include <amxprof/profiler.h>
+#include <amxprof/statistics_writer_html.h>
+#include <amxprof/statistics_writer_json.h>
+#include <amxprof/statistics_writer_text.h>
+#include "amxpath.h"
+#include "logprintf.h"
+#include "profiler.h"
+
+ConfigReader Profiler::server_cfg_("server.cfg");
+
+bool Profiler::profile_gamemode_ =
+  server_cfg_.GetOptionDefault("profile_gamemode", false);
+std::string Profiler::profile_filterscripts_ =
+  server_cfg_.GetOptionDefault<std::string>("profile_filterscripts", "");
+std::string Profiler::profile_format_ =
+  server_cfg_.GetOptionDefault<std::string>("profile_format", "html");
+bool Profiler::call_graph_ =
+  server_cfg_.GetOptionDefault("call_graph", false);
+std::string Profiler::call_graph_format_ =
+  server_cfg_.GetOptionDefault<std::string>("call_graph_format", "dot");
+
+#define logprintf Use_Printf_isntead_of_logprintf
+
+namespace {
+
+void Printf(const char *format, ...) {
+  std::va_list va;
+  va_start(va, format);
+
+  std::string new_format;
+  new_format.append("[profiler] ");
+  new_format.append(format);
+
+  vlogprintf(new_format.c_str(), va);
+  va_end(va);
+}
+
+void PrintException(const std::exception &e) {
+  Printf("Error: %s", e.what());
+}
+
+std::string GetAmxPath(AMX *amx) {
+  static AmxPathFinder finder;
+  finder.AddSearchDirectory("gamemodes");
+  finder.AddSearchDirectory("filterscripts");
+  return finder.FindAmxPath(amx);
+}
+
+void ToLower(std::string &s) {
+  std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+}
+
+std::string ToUnixPath(const std::string &path) {
+  std::string fsPath = path;
+  std::replace(fsPath.begin(), fsPath.end(), '\\', '/');
+  return fsPath;
+}
+
+bool IsGameMode(const std::string &amx_path) {
+  return amx_path.find("gamemodes/") != std::string::npos;
+}
+
+bool IsFilterScript(const std::string &amx_path) {
+  return amx_path.find("filterscripts/") != std::string::npos;
+}
+
+} // anonymous namespace
+
+int Profiler::Debug() {
+  if (profiler_ != 0) {
+    try {
+      return profiler_->DebugHook(prev_debug_);
+    } catch (const std::exception &e) {
+      PrintException(e);
+    }
+  }
+  if (prev_debug_ != 0) {
+    return prev_debug_(amx());
+  }
+  return AMX_ERR_NONE;
+}
+
+int Profiler::Callback(cell index, cell *result, cell *params) {
+  if (profiler_ != 0) {
+    try {
+      return profiler_->CallbackHook(index, result, params, prev_callback_);
+    } catch (const std::exception &e) {
+      PrintException(e);
+    }
+  }
+  if (prev_callback_ != 0) {
+    return prev_callback_(amx(), index, result, params);
+  }
+  return AMX_ERR_NONE;
+}
+
+int Profiler::Exec(cell *retval, int index) {
+  if (profiler_ != 0) {
+    try {
+      return profiler_->ExecHook(retval, index, amx_Exec);
+    } catch (const std::exception &e) {
+      PrintException(e);
+    }
+  }
+  return amx_Exec(amx(), retval, index);
+}
+
+Profiler::Profiler(AMX *amx)
+  : AMXService<Profiler>(amx),
+    prev_debug_(amx->debug),
+    prev_callback_(amx->callback),
+    profiler_(0),
+    debug_info_(0)
+{
+  try {
+    std::string amx_path = ToUnixPath(GetAmxPath(amx));
+    if (amx_path.empty()) {
+      Printf("Failed to find .amx file");
+      return;
+    }
+
+    bool attach_profiler = false;
+    if (IsGameMode(amx_path)) {
+      attach_profiler = profile_gamemode_;
+    } else if (IsFilterScript(amx_path)) {
+      std::stringstream fs_stream(profile_filterscripts_);
+      do {
+        std::string fs_name;
+        fs_stream >> fs_name;
+        if (amx_path == "filterscripts/" + fs_name + ".amx" ||
+            amx_path == "filterscripts/" + fs_name) {
+          attach_profiler = true;
+          break;
+        }
+      } while (!fs_stream.eof());
+    }
+    if (!attach_profiler) {
+      return;
+    }
+
+    if (amxprof::HasDebugInfo(amx)) {
+      debug_info_ = new amxprof::DebugInfo(amx_path);
+      if (!debug_info_->is_loaded()) {
+        Printf("Error loading debug info: %s",
+               aux_StrError(debug_info_->last_error()));
+        delete debug_info_;
+        debug_info_ = 0;
+      }
+    }
+
+    profiler_ = new amxprof::Profiler(amx, debug_info_);
+    profiler_->set_call_graph_enabled(call_graph_);
+
+    if (debug_info_ != 0) {
+      Printf("Attached profiler to '%s'", amx_path.c_str());
+    } else {
+      Printf("Attached profiler to '%s' (no debug info)", amx_path.c_str());
+    }
+  }
+  catch (const std::exception &e) {
+    PrintException(e);
+  }
+}
+
+Profiler::~Profiler() {
+  try {
+    if (profiler_ != 0) {
+      std::string amx_path = GetAmxPath(amx());
+      std::string amx_name = std::string(amx_path, 0,
+                                         amx_path.find_last_of("."));
+
+      ToLower(profile_format_);
+      std::string profile_filename = amx_name + "-profile." +
+                                     profile_format_;
+      std::ofstream profile_stream(profile_filename.c_str());
+
+      if (profile_stream.is_open()) {
+        amxprof::StatisticsWriter *writer = 0;
+
+        if (profile_format_ == "html") {
+          writer = new amxprof::StatisticsWriterHtml;
+        } else if (profile_format_ == "txt" ||
+                   profile_format_ == "text") {
+          writer = new amxprof::StatisticsWriterText;
+        } else if (profile_format_ == "json") {
+          writer = new amxprof::StatisticsWriterJson;
+        } else {
+          Printf("Unrecognized profile format '%s'", profile_format_.c_str());
+        }
+
+        if (writer != 0) {
+          Printf("Writing stats to '%s'", profile_filename.c_str());
+          writer->set_stream(&profile_stream);
+          writer->set_script_name(amx_path);
+          writer->set_print_date(true);
+          writer->set_print_run_time(true);
+          writer->Write(profiler_->stats());
+          delete writer;
+        }
+
+        profile_stream.close();
+      } else {
+        Printf("Error opening file '%s'", profile_filename.c_str());
+      }
+
+      if (call_graph_) {
+        ToLower(call_graph_format_);
+        std::string call_graph_filename = amx_name + "-calls." +
+                                          call_graph_format_;
+        std::ofstream call_graph_stream(call_graph_filename.c_str());
+
+        if (call_graph_stream.is_open()) {
+          amxprof::CallGraphWriterDot *writer = 0;
+
+          if (call_graph_format_ == "dot") {
+            writer = new amxprof::CallGraphWriterDot;
+          } else {
+            Printf("Unrecognized call graph format '%s'",
+                   call_graph_format_.c_str());
+          }
+
+          if (writer != 0) {
+            Printf("Writing call graph to '%s'", call_graph_filename.c_str());
+            writer->set_stream(&call_graph_stream);
+            writer->set_script_name(amx_path);
+            writer->set_root_node_name("SA-MP Server");
+            writer->Write(profiler_->call_graph());
+            delete writer;
+          }
+
+          call_graph_stream.close();
+        } else {
+          Printf("Error opening file '%s'", call_graph_filename.c_str());
+        }
+      }
+    }
+
+    delete profiler_;
+    delete debug_info_;
+  }
+  catch (const std::exception &e) {
+    PrintException(e);
+  }
+}

src/CMakeLists.txt
@@ -17,16 +17,21 @@ set(PLUGIN_SOURCES
   amxpath.cpp
   amxpath.h
   amxplugin.cpp
+  amxservice.h
   configreader.cpp
   configreader.h
   fileutils.cpp
   fileutils.h
+  logprintf.cpp
+  logprintf.h
   plugin.h
   plugincommon.h
   plugin.cpp
   plugin.def
   ${CMAKE_CURRENT_BINARY_DIR}/plugin.rc
   ${CMAKE_CURRENT_BINARY_DIR}/pluginversion.h
+  profiler.cpp
+  profiler.h
 )
 
 if(WIN32)

src/amxservice.h
@@ -1,0 +1,84 @@
+// Copyright (c) 2012-2014 Zeex
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef AMXSERVICE_H
+#define AMXSERVICE_H
+
+#include <map>
+
+#include <amx/amx.h>
+
+template<typename T>
+class AMXService {
+ public:
+  AMXService(AMX *amx) : amx_(amx) {}
+
+  AMX *amx() const { return amx_; }
+
+ public:
+  static T *CreateInstance(AMX *amx);
+  static T *GetInstance(AMX *amx);
+  static void DestroyInstance(AMX *amx);
+
+ private:
+  AMX *amx_;
+
+ private:
+  typedef std::map<AMX*, T*> ServiceMap;
+  static ServiceMap service_map_;
+};
+
+template<typename T>
+typename AMXService<T>::ServiceMap AMXService<T>::service_map_;
+
+// static
+template<typename T>
+T *AMXService<T>::CreateInstance(AMX *amx) {
+  T *service = new T(amx);
+  service_map_.insert(std::make_pair(amx, service));
+  return service;
+}
+
+// static
+template<typename T>
+T *AMXService<T>::GetInstance(AMX *amx) {
+  typename ServiceMap::const_iterator iterator = service_map_.find(amx);
+  if (iterator != service_map_.end()) {
+    return iterator->second;
+  }
+  return CreateInstance(amx);
+}
+
+// static
+template<typename T>
+void AMXService<T>::DestroyInstance(AMX *amx) {
+  typename ServiceMap::iterator iterator = service_map_.find(amx);
+  if (iterator != service_map_.end()) {
+    T *service = iterator->second;
+    service_map_.erase(iterator);
+    delete service;
+  }
+}
+
+#endif // !AMXSERVICE_H

src/logprintf.cpp
@@ -1,0 +1,43 @@
+// Copyright (c) 2012-2014 Zeex
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <cstdarg>
+#include <cstdio>
+
+#include "logprintf.h"
+
+#define LOGPRINTF_BUFFER_SIZE 1024
+
+#ifdef _MSC_VER
+  #define vsnprintf vsprintf_s
+#endif
+
+logprintf_t logprintf;
+
+void vlogprintf(const char *format, va_list va) {
+  char buffer[LOGPRINTF_BUFFER_SIZE];
+  vsnprintf(buffer, sizeof(buffer), format, va);
+  buffer[sizeof(buffer) - 1] = '\0';
+  logprintf("%s", buffer);
+}

commit 36b1bc8cf3a80e7b18169a93a90f450ac724b8ee

Fix performance regression
Don't set the debug hook and callback for a script if profiler is
not enabled for it.

This fixes a performance regression in that was introduced in commit
bcd13beffbe8bef1db5be90e96038230eb3e2ab6.

src/plugin.cpp
@@ -85,9 +85,10 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx) {
   Profiler::CreateInstance(amx);
-  Profiler::GetInstance(amx)->Load();
-  amx_SetDebugHook(amx, hooks::amx_Debug);
-  amx_SetCallback(amx, hooks::amx_Callback);
+  if (Profiler::GetInstance(amx)->Load() == AMX_ERR_NONE) {
+    amx_SetDebugHook(amx, hooks::amx_Debug);
+    amx_SetCallback(amx, hooks::amx_Callback);
+  }
   return AMX_ERR_NONE;
 }
 

src/profiler.cpp
@@ -23,6 +23,7 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 #include <algorithm>
+#include <cassert>
 #include <cstdarg>
 #include <exception>
 #include <fstream>
@@ -97,43 +98,46 @@ bool IsFilterScript(const std::string &amx_path) {
 
 int Profiler::Load() {
   try {
-    std::string amx_path = ToUnixPath(GetAmxPath(amx()));
-    if (amx_path.empty()) {
-      Printf("Failed to find .amx file");
-      return AMX_ERR_NONE;
-    }
-
-    if (IsGameMode(amx_path)) {
-      is_attached_ = profile_gamemode_;
-    } else if (IsFilterScript(amx_path)) {
-      std::stringstream fs_stream(profile_filterscripts_);
-      do {
-        std::string fs_name;
-        fs_stream >> fs_name;
-        if (amx_path == "filterscripts/" + fs_name + ".amx" ||
-            amx_path == "filterscripts/" + fs_name) {
-          is_attached_ = true;
-          break;
-        }
-      } while (!fs_stream.eof());
-    }
     if (!is_attached_) {
-      return AMX_ERR_NONE;
-    }
-
-    if (amxprof::HasDebugInfo(amx())) {
-      debug_info_.Load(amx_path);
-      if (!debug_info_.is_loaded()) {
-        Printf("Error loading debug info: %s",
-               aux_StrError(debug_info_.last_error()));
+      std::string amx_path = ToUnixPath(GetAmxPath(amx()));
+      if (amx_path.empty()) {
+        Printf("Failed to find .amx file");
+        return AMX_ERR_GENERAL;
       }
-    }
 
-    if (debug_info_.is_loaded()) {
-      profiler_.set_debug_info(&debug_info_);
-      Printf("Attached profiler to %s", amx_path.c_str());
-    } else {
-      Printf("Attached profiler to %s (no debug info)", amx_path.c_str());
+      if (IsGameMode(amx_path)) {
+        is_attached_ = profile_gamemode_;
+      } else if (IsFilterScript(amx_path)) {
+        std::stringstream fs_stream(profile_filterscripts_);
+        do {
+          std::string fs_name;
+          fs_stream >> fs_name;
+          if (amx_path == "filterscripts/" + fs_name + ".amx" ||
+              amx_path == "filterscripts/" + fs_name) {
+            is_attached_ = true;
+            break;
+          }
+        } while (!fs_stream.eof());
+      }
+      if (!is_attached_) {
+        return AMX_ERR_GENERAL;
+      }
+
+      if (amxprof::HasDebugInfo(amx())) {
+        debug_info_.Load(amx_path);
+        if (debug_info_.is_loaded()) {
+          profiler_.set_debug_info(&debug_info_);
+        } else {
+          const char *error = aux_StrError(debug_info_.last_error());
+          Printf("Error loading debug info: %s", error);
+        }
+      }
+
+      if (debug_info_.is_loaded()) {
+        Printf("Attached profiler to %s", amx_path.c_str());
+      } else {
+        Printf("Attached profiler to %s (no debug info)", amx_path.c_str());
+      }
     }
   }
   catch (const std::exception &e) {